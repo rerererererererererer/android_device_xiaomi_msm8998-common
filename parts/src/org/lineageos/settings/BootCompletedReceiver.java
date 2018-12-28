35161113154c25013a8f1790290de46d597d9381
TheScarastic <warabhishek@gmail.com>
2018-12-28 15:13:24 -0500

    msm8998-common: parts: Add DiracSound backend
    Special thanks to vasishath for his base work on mi3
    
    Change-Id: Id81e60a18dc34c991b82a5a2685c9041222a6ea7
    Signed-off-by: PainKiller3 <ninadpatil100@gmail.com>

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,6 +22,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import org.lineageos.settings.dirac.DiracUtils;
+import org.lineageos.settings.doze.DozeUtils;
+
+
 public class BootCompletedReceiver extends BroadcastReceiver {
 
     private static final boolean DEBUG = false;
@@ -31,6 +35,7 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DozeUtils.isDozeEnabled(context) && DozeUtils.sensorsEnabled(context)) {
             if (DEBUG) Log.d(TAG, "Starting service");
+            DiracUtils.initialize();
             DozeUtils.startService(context);
         }
     }
