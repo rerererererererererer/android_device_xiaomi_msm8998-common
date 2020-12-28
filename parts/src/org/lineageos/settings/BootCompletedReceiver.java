bc470e6547badfa756e554baf90c19fee945754e
rerererererererererer <thesupernikkce@yandex.ru>
2020-12-28 11:53:10 +0300

    msm8998-common: Nuke Xiaomi Doze

--- parts/src/org/lineageos/settings/BootCompletedReceiver.java
+++ parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -22,7 +22,6 @@ import android.content.Intent;
 import android.util.Log;
 
 import org.lineageos.settings.dirac.DiracUtils;
-import org.lineageos.settings.doze.DozeUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
 
 
@@ -35,7 +34,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
     public void onReceive(final Context context, Intent intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         DiracUtils.initialize();
-        DozeUtils.startService(context);
         ThermalUtils.startService(context);
     }
 }
