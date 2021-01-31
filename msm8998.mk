5073bc716ccfeed1ac07f2fee332188ee0fb5388
rerererererererererer <thesupernikkce@yandex.ru>
2021-02-01 01:40:03 +0300

    Revert "msm8998-common: Enable LED based vibrator"
    This reverts commit 23b7d22da6eb5d8c8fcf7da42e4e4507234687b0.

--- msm8998.mk
+++ msm8998.mk
@@ -393,10 +393,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
-# Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
